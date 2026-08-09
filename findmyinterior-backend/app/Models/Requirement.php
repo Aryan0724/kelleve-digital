f7da5ee0db3f4db328a8abc923e884039b840a96
Aryan0724 <aryantiwari0724@gmail.com>
2026-08-09 19:18:56 +0530

    fix: remove duplicate images() method in Requirement.php

--- findmyinterior-backend/app/Models/Requirement.php
+++ findmyinterior-backend/app/Models/Requirement.php
@@ -87,10 +87,6 @@ class Requirement extends Model
         return $this->belongsTo(District::class);
     }
 
-    public function images(): HasMany
-    {
-        return $this->hasMany(RequirementImage::class);
-    }
 
     public function contactUnlocks(): HasMany
     {
