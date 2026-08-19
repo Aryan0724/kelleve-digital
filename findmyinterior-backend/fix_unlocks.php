44327df152765b6d570dc5f92fb77f80a7217f3d
Aryan0724 <aryantiwari0724@gmail.com>
2026-08-19 14:36:50 +0530

    chore: output unlocks

--- findmyinterior-backend/fix_unlocks.php
+++ findmyinterior-backend/fix_unlocks.php
@@ -5,14 +5,5 @@ $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 
 use App\Models\ContactUnlock;
-use Illuminate\Support\Facades\DB;
 
-$affected = ContactUnlock::where('requirement_type', 'App\Models\Requirement')
-    ->whereNotExists(function ($query) {
-        $query->select(DB::raw(1))
-            ->from('projects')
-            ->whereColumn('projects.id', 'contact_unlocks.requirement_id');
-    })
-    ->update(['requirement_type' => 'App\Models\WorkerJob']);
-
-echo "Fixed $affected records.\n";
+echo json_encode(ContactUnlock::all());
